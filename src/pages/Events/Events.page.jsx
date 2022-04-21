860aa4f5354cd7736c7ac1bdfb27c2109bddcd3b
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-04-21 13:13:43 +0530

    Changes 21.04.2022pm

--- src/pages/Events/Events.page.jsx
+++ src/pages/Events/Events.page.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import Navbar from "../../components/Navbar/Navbar.component";
+import Threed from "./threed";
 
 const EventsPage = () => {
-  const loader = new GLTFLoader();
-
-  loader.load(
-    "path/to/model.glb",
-    function (gltf) {
-      scene.add(gltf.scene);
-    },
-    undefined,
-    function (error) {
-      console.error(error);
-    }
-  );
-  
   return (
-    <>
-      <Navbar />
-      <div></div>
-    </>
+    <Threed />
   );
 };
 
