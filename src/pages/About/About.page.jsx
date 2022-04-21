76b34ac43cf5113ad2786770ca536df3ab106c0d
solomonsanthosh <solosanty2064@gmail.com>
2022-04-21 14:26:40 +0530

    SS

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
-
+import { useRef, useEffect } from "react";
+import { useLayoutEffect } from 'react';
+import './about.css'
+import { gsap } from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger.js";
 import Navbar from '../../components/Navbar/Navbar.component';
 import { HeroSection } from './About.styles';
 const AboutPage = () => {
+	
+	// const handleScroll = (e) => {
+	gsap.registerPlugin(ScrollTrigger)
+	// 	const scrollY = window.scrollY
+	// 	console.log('====================================');
+	// 	console.log(scrollY);
+	// 	console.log('====================================');
+	// }
+	// window.addEventListener('scroll', handleScroll);
+	const tab = useRef();
+	useEffect(() => {
+		let t1 = gsap.timeline();
+		const element = tab.current;
+		
+			t1.to(element, {
+				scrollTrigger: {
+					trigger: element,
+					
+					pin: true,
+					start: 'top 50%',
+            		end: 'bottom -100%',
+					// scroller: window,
+					// scroller: window,
+					scrub: true,
+					markers: true,
+					pinSpacer: true,
+					pinSpacing: false,
+	
+	
+					
+				}
+			})
+			
+		ScrollTrigger.refresh()
+      t1.to( element.querySelector('.para') , {
+        y: 0,
+		
+      })
+	  
+	},[]);
 	return (
 		<>
 			<Navbar />
 			<HeroSection>
-				<div>
+				<div ref={tab} className='tab' >
+					<span className="speaker"></span>
+					<span className='cam1'></span>
+					<span className='cam2'></span>
 					<h1>We Introduce Ourselves.</h1>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ipsam, quos aspernatur qui corporis
+					<p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ipsam, quos aspernatur qui corporis
             odit. Aut voluptas rerum minima veniam natus molestiae. Omnis sit maxime exercitationem hic consequatur
             corrupti ratione. Quaerat repellat corporis ab enim dolorum veniam consectetur suscipit totam nisi quo
             doloribus perspiciatis officiis non, molestias libero quisquam reiciendis harum? Aut, minus voluptates? Sit
--- src/pages/About/About.styles.js
+++ src/pages/About/About.styles.js
@@ -3,26 +3,61 @@ import styled from "styled-components";
 export const HeroSection = styled.section`
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap');
 
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 200vh;
   background: black;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
   align-items: center;
   overflow: hidden;
   position: relative;
   div {
-    width: 80%;
-    height: 75%;
+    // width: 80%;
+    height: 80vh;
+    
     border-radius: 15px;
     border: 5px solid #30AADD;
     padding:3rem;
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+    // position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: left;
+    align-items: center;
   }
+  .speaker {
+    position: absolute;
+    top: 50%;
+    left:2%;
+    transform: translateY(-50%);
+    height: 80px;
+    width: 10px;
+    border: 3px solid #30AADD;
+    border-radius: 20%;
+
+  }
+  .cam1 {
+    position: absolute;
+    bottom : 12%;
+      left: 1.5%;
+      height: 20px;
+      width: 20px;
+      border-radius: 50%;
+      border: 5px solid #30AADD;
+  }
+  .cam2 {
+    position: absolute;
+    bottom : 3%;
+      left: 1%;
+      height: 30px;
+      width: 30px;
+      border-radius: 50%;
+      border: 5px solid #30AADD;
+
+}
+  
   h1 {
     font-family: 'Montserrat', sans-serif;
     font-size: 2.5rem;
@@ -31,14 +66,18 @@ export const HeroSection = styled.section`
     color: #fff;
     /* padding: 5% 10%; */
     padding-bottom: 3rem;
+    // margin-left:6%;
     z-index: 2;
+    width:90%;
   }
   p {
     font-family: 'Montserrat', sans-serif;
-    width:100%;
+    width:90%;
     font-size: 1.2rem;
     line-height: 2.3rem;
     color: white;
+    // margin-left:6%;
+    transform: translateY(50%);
     font-weight: 300;
   }
   
