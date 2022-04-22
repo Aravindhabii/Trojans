3e4e8e92d88f88a4c9241b49190b1db66181461e
Prasanna <prasannavelmurugan0200@gmail.com>
2022-04-22 23:53:28 +0530

    rv

--- src/pages/Registration/Registration.page.jsx
+++ src/pages/Registration/Registration.page.jsx
@@ -3,33 +3,17 @@ import {Navbar} from "../../components/Navbar/Navbar.component";
 import Form from "../../components/Registration/Form";
 // import Boxes from "../../components/Registration/Boxes";
 // import {Container} from "../../styles/container.style";
-import Video from "../../assets/Video.mp4"
+import Shadow from "../../components/Registration/Shadow";
+import Video from "../../assets/Video.mp4";
 import "./Registration.css";
 
 function Registration() {
     return (
         <section className="section-registration">
-            {/* <div className="box-main-div">
-                <Boxes />
-            </div> */}
-            <div className="video-bg">
-                <video>
-                    <source src="Video" />
-                </video>
-                <Form />
-            </div>
+            <Shadow />
+            <Form />
         </section>
     );
 }
-
-// name
-// phone
-// email
-// clg
-// dapartment
-// qr code / gpay no
-// technical events
-// non technical events
-// workshop
 
 export default Registration;
