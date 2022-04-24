f8df87bb4141b16f3e56fcc3501ca06920022091
Lohith <srklohith05@gmail.com>
2022-04-24 10:09:58 +0530

    chnages

--- src/pages/Registration/Registration.page.jsx
+++ src/pages/Registration/Registration.page.jsx
@@ -3,17 +3,20 @@ import Form from '../../components/Registration/Form';
 import Navbar from '../../components/Navbar/Navbar.component';
 // import Boxes from "../../components/Registration/Boxes";
 // import {Container} from "../../styles/container.style";
-import Shadow from "../../components/Registration/Shadow";
-import Video from "../../assets/Video.mp4";
-import "./Registration.css";
+import Shadow from '../../components/Registration/Shadow';
+import Video from '../../assets/Video.mp4';
+import './Registration.css';
 
 function Registration() {
-    return (
-        <section className="section-registration">
-            {/* <Shadow /> */}
-            <Form />
-        </section>
-    );
+	return (
+		<>
+			<Navbar active={{ route: 'registration', scroll: 4 }} />
+			<section className='section-registration'>
+				{/* <Shadow /> */}
+				<Form />
+			</section>
+		</>
+	);
 }
 
 export default Registration;
