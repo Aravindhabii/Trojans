5ef3880a1d1cfbf674cec281b3bd85508affc235
solomonsanthosh <solosanty2064@gmail.com>
2022-04-27 13:04:08 +0530

    SS

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -12,7 +12,7 @@ import btn1 from '../../assets/about/btn1.png';
 import btn2 from '../../assets/about/btn2.png';
 const AboutPage = () => {
 	const [display, setDisplay] = useState(1);
-	
+	const [contentTrigger,setContentTrigger] = useState(false)
 	gsap.registerPlugin(ScrollTrigger)
 
 	const handleScroll = (e) => {
@@ -30,6 +30,9 @@ const AboutPage = () => {
 	window.addEventListener('scroll', handleScroll);
 	const tab = useRef();
 	useEffect(() => {
+		if(window.innerWidth < '780') {
+			setContentTrigger(true)
+		}
 		let t1 = gsap.timeline();
 		const element = tab.current;
 		
@@ -55,7 +58,7 @@ const AboutPage = () => {
 		ScrollTrigger.refresh()
       t1.to( element.querySelectorAll('.para') , {
         y: 0,
-		ease: "easInOut",
+		ease: "easeInOut",
 		duration: 1,
       })
 	  
@@ -71,16 +74,17 @@ const AboutPage = () => {
 	return (
 		<>
 			<Navbar active={{ route: 'about', scroll: 1 }} />
-			<HeroSection>
+			<HeroSection contentTrigger={contentTrigger}>
 				<video className='video' loop autoPlay muted>
 					<source src={video} type="video/mp4"/>
 				</video>
 				<div ref={tab} className='tab' >
-					<span><img src={btn1} className='btn1' onClick={()=>handleClick('goDown')}></img></span>
-					<span><img src={btn2} className='btn2'onClick={()=>handleClick('goBack')}></img></span>
+					<span className='btn1' onClick={()=>handleClick('goDown')}></span>
+					<span className='btn2' onClick={()=>handleClick('goBack')}></span>
 					<span className="speaker"></span>
 					<span className='cam1'></span>
 					<span className='cam2'></span>
+					<span className='cam3'></span>
 					{display == 1 ? <p className='para para1'>
 					<h1>We Introduce Ourselves.</h1>
 
@@ -88,10 +92,10 @@ const AboutPage = () => {
             odit. Aut voluptas rerum minima veniam natus molestiae. Omnis sit maxime exercitationem hic consequatur
             corrupti ratione. Quaerat repellat corporis ab enim dolorum veniam consectetur suscipit totam nisi quo
             doloribus perspiciatis officiis non, molestias libero quisquam reiciendis harum? Aut, minus voluptates? Sit
-            optio ex officia consequatur non saepe error excepturi, quas autem, exercitationem atque. Voluptatum quidem
+            optio ex officia consequatur non saepe error excepturi, quas autem, exercitationem atque.<span className='contentextra'>Voluptatum quidem
             iusto molestias, consequuntur fugiat possimus minima quasi aliquam! Necessitatibus obcaecati doloribus
             dolores vel ut autem numquam. Autem, earum delectus mollitia natus blanditiis laborum in cumque sequi dolore
-            ut neque, et minus.</p> : null}
+            ut neque, et minus.</span></p> : null}
 			{display == 2 ? <p className='para para2'>
 			<h1>screen2</h1>
 				santy sit amet consectetur adipisicing elit. Recusandae ipsam, quos aspernatur qui corporis
