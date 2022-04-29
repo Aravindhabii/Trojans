2f558cb698c17395ac1298c60cfad227e7356af2
Prasanna <prasannavelmurugan0200@gmail.com>
2022-04-29 11:44:55 +0530

    rv

--- src/pages/Guidelines/Guidelines.page.jsx
+++ src/pages/Guidelines/Guidelines.page.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from '../../components/Navbar/Navbar.component';
-import FirstSection from '../../components/Guidelines/FirstSection';
+import Navbar from "../../components/Navbar/Navbar.component";
+import FirstSection from "../../components/Guidelines/FirstSection";
+import Footer from "../../components/Footer/Footer";
 
 const GuidelinesPage = () => {
-	return (
-		<>
-			<Navbar active={{ route: 'guidelines', scroll: 3 }} />
-			<FirstSection />
-		</>
-	);
+    return (
+        <>
+            <Navbar active={{route: "guidelines", scroll: 3}} />
+            <FirstSection />
+            {/* <Footer /> */}
+        </>
+    );
 };
 
 export default GuidelinesPage;
