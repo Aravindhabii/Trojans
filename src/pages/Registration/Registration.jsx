e276fc405839eb0a5b59f81132c5b194f193b25d
Prasanna <prasannavelmurugan0200@gmail.com>
2022-04-30 11:00:41 +0530

    rv

---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistrationComponent from "../../components/Registration/RegistrationComponent";
+
+function Registration() {
+    return (
+        <>
+            <RegistrationComponent />
+        </>
+    );
+}
+
+export default Registration;
