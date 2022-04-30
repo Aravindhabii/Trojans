fe9a568f78dd5c3441ba173c75071eec1c1420c6
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-04-30 11:00:13 +0530

    30.04.2022 10.50pm aravindh

--- src/pages/Events/Events.page.jsx
+++ src/pages/Events/Events.page.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 
-import Navbar from '../../components/Navbar/Navbar.component';
-import Threed from './threed';
+import Navbar from "../../components/Navbar/Navbar.component";
+import Threed from "./threed";
 import {
-	HeroSection,
-	Button,
-	Threeddiv,
-	Righttwo,
-	Rightone,
-	Tabletdiv
-} from './Events.styles';
-import { CodersChemistry, MasterMinds } from '../../datas/events.data';
+  HeroSection,
+  Button,
+  Threeddiv,
+  Righttwo,
+  Rightone,
+  Tabletdiv,
+} from "./Events.styles";
+import { CodersChemistry, MasterMinds } from "../../datas/events.data";
 
-import Technical from '../../components/events/Technical';
-import Tablet from '../../components/events/Tablet';
-import { technicalevents } from '../../datas/technical.data';
+import Technical from "../../components/events/Technical";
+import Tablet from "../../components/events/Tablet";
+import { technicalevents } from "../../datas/technical.data";
 
 const EventsPage = () => {
   const [currentPage, setCurrentPage] = useState(technicalevents);
   const [isphoneopen, setisphoneopen] = useState(false);
   const [currentEvent, setCurrentEvent] = useState(CodersChemistry);
-  const [clickedEvent, setClickedEvent] = useState('');
+  const [clickedEvent, setClickedEvent] = useState("");
   const [isTabletOpen, setisTabletOpen] = useState(false);
 
   const closetab = () => {
@@ -43,81 +43,41 @@ const EventsPage = () => {
       setisphoneopen(false);
       setClickedEvent("");
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('clicked useeffect');
-    if(clickedEvent === 'Coders Chemistry'){
+    console.log("clicked useeffect");
+    if (clickedEvent === "Coders Chemistry") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Master Minds'){
-      console.log('Master Minds');
+    } else if (clickedEvent === "Master Minds") {
+      console.log("Master Minds");
       setCurrentEvent(MasterMinds);
-    }else if(clickedEvent === 'Site-ing'){
+    } else if (clickedEvent === "Site-ing") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Paper Bytes'){
+    } else if (clickedEvent === "Paper Bytes") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Goose chase'){
+    } else if (clickedEvent === "Goose chase") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === "Trojan's Throttle"){
+    } else if (clickedEvent === "Trojan's Throttle") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Technical'){
+    } else if (clickedEvent === "Technical") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Technical'){
+    } else if (clickedEvent === "Technical") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Technical'){
+    } else if (clickedEvent === "Technical") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Technical'){
+    } else if (clickedEvent === "Technical") {
       setCurrentEvent(CodersChemistry);
-    }else if(clickedEvent === 'Technical'){
+    } else if (clickedEvent === "Technical") {
       setCurrentEvent(CodersChemistry);
     }
-
   }, [clickedEvent]);
-
-
-
-	const closetab = () => {
-		setisTabletOpen(false);
-	};
-	const click = (e) => {
-		if (e.target.value !== clickedEvent) {
-			setClickedEvent(e.target.value);
-			setisphoneopen(true);
-		} else {
-			setisphoneopen(false);
-			setClickedEvent('');
-		}
-	};
-	useEffect(() => {
-		console.log('clicked useeffect');
-		if (clickedEvent === 'Coders Chemistry') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Master Minds') {
-			console.log('Master Minds');
-			setCurrentEvent(MasterMinds);
-		} else if (clickedEvent === 'Site-ing') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Paper Bytes') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Goose chase') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === "Trojan's Throttle") {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Technical') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Technical') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Technical') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Technical') {
-			setCurrentEvent(CodersChemistry);
-		} else if (clickedEvent === 'Technical') {
-			setCurrentEvent(CodersChemistry);
-		}
-	}, [clickedEvent]);
 
   return (
     <>
+      <Helmet>
+        <title>EVENTS | TROJANS</title>
+      </Helmet>
       <Navbar active={{ route: "events", scroll: 2 }} />
       <HeroSection>
         <div className="left">
@@ -138,13 +98,22 @@ const EventsPage = () => {
             </Threeddiv>
           </Rightone>
           <Righttwo openorclose={isphoneopen}>
-            <Technical setCurrentEvent={setCurrentEvent} currentEvent={currentEvent} currentPage={currentPage} setCurrentPage={setCurrentPage} clickedEvent={clickedEvent} setClickedEvent={setClickedEvent} isTabletOpen={isTabletOpen} setisTabletOpen={setisTabletOpen}/>
+            <Technical
+              setCurrentEvent={setCurrentEvent}
+              currentEvent={currentEvent}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              clickedEvent={clickedEvent}
+              setClickedEvent={setClickedEvent}
+              isTabletOpen={isTabletOpen}
+              setisTabletOpen={setisTabletOpen}
+            />
           </Righttwo>
         </div>
       </HeroSection>
-      <Tabletdiv  isTabletOpen={isTabletOpen}  setisTabletOpen={setisTabletOpen}>
-          <div onClick={closetab}  className="forclose"></div>
-          <Tablet currentEvent={currentEvent}/>
+      <Tabletdiv isTabletOpen={isTabletOpen} setisTabletOpen={setisTabletOpen}>
+        <div onClick={closetab} className="forclose"></div>
+        <Tablet currentEvent={currentEvent} />
       </Tabletdiv>
     </>
   );
