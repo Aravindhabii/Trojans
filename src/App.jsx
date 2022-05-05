3ef6f7b77f7713e4adb737857dc81b0ead74c8f1
Prasanna <prasannavelmurugan0200@gmail.com>
2022-05-05 19:28:06 +0530

    rv

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,115 @@
-import React, { useRef, useContext, lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { GlobalStyles } from './global.style.js';
-import { ThemeProvider } from 'styled-components';
-import { ToastContainer } from 'react-toastify';
-import ThemeContext from './Theme.context.jsx';
-import 'react-toastify/dist/ReactToastify.css';
+import React, {useRef, useContext, lazy, Suspense} from "react";
+import {Routes, Route} from "react-router-dom";
+import {GlobalStyles} from "./global.style.js";
+import {ThemeProvider} from "styled-components";
+import {ToastContainer} from "react-toastify";
+import ThemeContext from "./Theme.context.jsx";
+import "react-toastify/dist/ReactToastify.css";
 
-import Trojans from './assets/Trojans_logo/trojans-compressed.png';
+import Trojans from "./assets/Trojans_logo/trojans-compressed.png";
 
-const Sponser = lazy(() => import('./pages/Sponser/Sponser.page'));
-const Cursor = lazy(() => import('./components/Cursor/Cursor.component.jsx'));
-const HomePage = lazy(() => import('./pages/Home/Home.page.jsx'));
-const EventsPage = lazy(() => import('./pages/Events/Events.page'));
-const GuidelinesPage = lazy(() => import('./pages/Guidelines/Guidelines.page'));
+const Sponsor = lazy(() => import("./pages/Sponsor/Sponsor.page"));
+const Cursor = lazy(() => import("./components/Cursor/Cursor.component.jsx"));
+const HomePage = lazy(() => import("./pages/Home/Home.page.jsx"));
+const EventsPage = lazy(() => import("./pages/Events/Events.page"));
+const GuidelinesPage = lazy(() => import("./pages/Guidelines/Guidelines.page"));
 const RegistrationPage = lazy(() =>
-	import('./pages/Registration/Registration.page')
+    import("./pages/Registration/Registration.page")
 );
 
 const App = () => {
-	const theme = useContext(ThemeContext);
-	// const [showCountdown, setShowCountdown] = useState(true);
-	const cursorRef = useRef();
+    const theme = useContext(ThemeContext);
+    // const [showCountdown, setShowCountdown] = useState(true);
+    const cursorRef = useRef();
 
-	return (
-		<Suspense
-			fallback={
-				<div
-					style={{
-						width: '100%',
-						height: '100vh',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'center',
-						backgroundColor: 'black',
-						padding: 0,
-						margin: 0,
-						boxSizing: 'border-box'
-					}}
-				>
-					<img src={Trojans} style={{ width: '10rem' }} alt='Trojans' />
-				</div>
-			}
-		>
-			<ThemeProvider theme={theme}>
-				<div
-					onMouseMove={(e) =>
-						(document.querySelector(
-							'.cursor'
-						).style.transform = `translate(${e.clientX}px, ${e.clientY}px)`)
-					}
-				>
-					<ToastContainer
-						position='top-right'
-						autoClose={5000}
-						hideProgressBar
-						newestOnTop
-						closeOnClick
-						rtl={false}
-						pauseOnFocusLoss
-						draggable
-						pauseOnHover
-						theme='dark'
-					/>
-					<Cursor cursorRef={cursorRef} />
-					<GlobalStyles />
-					{/* {showCountdown ? (
+    return (
+        <Suspense
+            fallback={
+                <div
+                    style={{
+                        width: "100%",
+                        height: "100vh",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: "black",
+                        padding: 0,
+                        margin: 0,
+                        boxSizing: "border-box",
+                    }}
+                >
+                    <img src={Trojans} style={{width: "10rem"}} alt="Trojans" />
+                </div>
+            }
+        >
+            <ThemeProvider theme={theme}>
+                <div
+                    onMouseMove={(e) =>
+                        (document.querySelector(
+                            ".cursor"
+                        ).style.transform = `translate(${e.clientX}px, ${e.clientY}px)`)
+                    }
+                >
+                    <ToastContainer
+                        position="top-right"
+                        autoClose={5000}
+                        hideProgressBar
+                        newestOnTop
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="dark"
+                    />
+                    <Cursor cursorRef={cursorRef} />
+                    <GlobalStyles />
+                    {/* {showCountdown ? (
 					<Countdown setShowCountdown={setShowCountdown} />
 				) : ( */}
-					<Routes>
-						<Route path='/' element={<HomePage />} />
-						<Route exact path='/events' element={<EventsPage page='' />} />
-						<Route
-							exact
-							path='/workshop'
-							element={<EventsPage page='workshops' />}
-						/>
-						<Route
-							exact
-							path='/nontechnical'
-							element={<EventsPage page='nontechnicalevents' />}
-						/>
-						<Route
-							exact
-							path='/technical'
-							element={<EventsPage page='technicalevents' />}
-						/>
-						<Route
-							exact
-							path='/gamming'
-							element={<EventsPage page='gamming' />}
-						/>
-						<Route exact path='/guidelines' element={<GuidelinesPage />} />
-						<Route exact path='/registration' element={<RegistrationPage />} />
-						<Route exact path='/sponsor' element={<Sponsor />} />
-					</Routes>
-					{/* )} */}
-				</div>
-			</ThemeProvider>
-		</Suspense>
-	);
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route
+                            exact
+                            path="/events"
+                            element={<EventsPage page="" />}
+                        />
+                        <Route
+                            exact
+                            path="/workshop"
+                            element={<EventsPage page="workshops" />}
+                        />
+                        <Route
+                            exact
+                            path="/nontechnical"
+                            element={<EventsPage page="nontechnicalevents" />}
+                        />
+                        <Route
+                            exact
+                            path="/technical"
+                            element={<EventsPage page="technicalevents" />}
+                        />
+                        <Route
+                            exact
+                            path="/gamming"
+                            element={<EventsPage page="gamming" />}
+                        />
+                        <Route
+                            exact
+                            path="/guidelines"
+                            element={<GuidelinesPage />}
+                        />
+                        <Route
+                            exact
+                            path="/registration"
+                            element={<RegistrationPage />}
+                        />
+                        <Route exact path="/sponsor" element={<Sponsor />} />
+                    </Routes>
+                    {/* )} */}
+                </div>
+            </ThemeProvider>
+        </Suspense>
+    );
 };
 export default App;
