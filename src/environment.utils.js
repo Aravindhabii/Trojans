bcdbeb833869066e461714c24f3b21d6050db398
Lohith <srklohith05@gmail.com>
2022-05-05 09:36:05 +0530

    cahnges

--- src/environment.utils.js
+++ src/environment.utils.js
@@ -2,12 +2,12 @@ const __DEV__ = document.domain === 'localhost';
 
 export const BACKEND_BASE_URL = __DEV__
 	? 'http://localhost:8080/api'
-	: 'https://trojans-server.herokuapp.com/api';
+	: 'https://trojanscit.com/api';
 
 export const REACT_BASE_URL = __DEV__
 	? 'http://localhost:3000'
-	: 'https://trojans-cit.herokuapp.com';
+	: 'https://trojanscit.com';
 
-export const RAZORPAY_KEY = __DEV__
+export const RAZORPAY_KEY = !__DEV__
 	? 'rzp_test_o2U8BQRS1aZ9Ww'
 	: 'rzp_live_ntnN5NKmqdPeFG';
