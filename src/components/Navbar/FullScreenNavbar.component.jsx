3f7b4b5f525d988196797a18ac71056fa163e315
Lohith <srklohith05@gmail.com>
2022-05-08 10:14:12 +0530

    changes

--- src/components/Navbar/FullScreenNavbar.component.jsx
+++ src/components/Navbar/FullScreenNavbar.component.jsx
@@ -10,6 +10,13 @@ import {
 	HoverContact
 } from './FullScreenNavbar.style';
 
+import img1 from '../../assets/nav/pic1.jpg';
+import img2 from '../../assets/nav/pic2.jpg';
+import img3 from '../../assets/nav/pic3.jpg';
+import img4 from '../../assets/nav/pic4.jpg';
+import img5 from '../../assets/nav/pic5.jpg';
+import img6 from '../../assets/nav/pic6.jpg';
+
 const NavLinkComponent = ({
 	body,
 	url,
@@ -40,14 +47,20 @@ const FullScreenNavbar = ({ active, isNavOpen, setIsNavOpen }) => {
 	}, [isNavOpen]);
 
 	const images = [
-		'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80',
-		'https://images.pexels.com/photos/11041919/pexels-photo-11041919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-		'https://images.pexels.com/photos/5818628/pexels-photo-5818628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-		'https://images.pexels.com/photos/8910626/pexels-photo-8910626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+		img2,
+		img3,
+		img1,
+		img4,
+		img5,
+		img6,
+		img2
+		// 'https://images.pexels.com/photos/11041919/pexels-photo-11041919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+		// 'https://images.pexels.com/photos/5818628/pexels-photo-5818628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+		// 'https://images.pexels.com/photos/8910626/pexels-photo-8910626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 	];
 
 	const [scrollDiv, setScrollDiv] = useState(active.scroll);
