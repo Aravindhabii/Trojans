3d8d3110edcec61d856e5b062fb4d722aff4990e
Lohith <srklohith05@gmail.com>
2022-05-08 09:52:19 +0530

    changes

--- src/axios/registration.axios.js
+++ src/axios/registration.axios.js
@@ -1,8 +1,26 @@
 import axios from 'axios';
-
 import { BACKEND_BASE_URL } from '../environment.utils';
 
-export const registrationEventAxios = (
+const uploadImage = async (image) => {
+	const data = new FormData();
+	data.append('file', image);
+	data.append('upload_preset', 'crtkv5rb');
+	data.append('cloud_name', 'trojanscit');
+	return await fetch(
+		'  https://api.cloudinary.com/v1_1/trojanscit/image/upload',
+		{
+			method: 'post',
+			body: data
+		}
+	)
+		.then((resp) => resp.json())
+		.then((data) => {
+			return data.url;
+		})
+		.catch((err) => console.log(err));
+};
+
+export const registrationEventAxios = async (
 	name,
 	email,
 	phone,
@@ -11,9 +29,16 @@ export const registrationEventAxios = (
 	college,
 	event,
 	workshops,
-	gaming
-) =>
-	axios.post(`${BACKEND_BASE_URL}/data`, {
+	gaming,
+	uploadPaper,
+	uploadAbstract,
+	uploadPresentation
+) => {
+	const paper = await uploadImage(uploadAbstract).then((url) => url);
+	const abstract = await uploadImage(uploadPresentation).then((url) => url);
+	const presentation = await uploadImage(uploadPaper).then((url) => url);
+
+	return axios.post(`${BACKEND_BASE_URL}/data`, {
 		name,
 		email,
 		phone,
@@ -22,5 +47,9 @@ export const registrationEventAxios = (
 		college,
 		event,
 		workshops,
-		gaming
+		gaming,
+		paper,
+		abstract,
+		presentation
 	});
+};
