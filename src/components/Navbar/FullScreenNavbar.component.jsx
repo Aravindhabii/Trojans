9c4ae0ca36452a59ec771c07b40cc6e5d7126417
Prasanna <prasannavelmurugan0200@gmail.com>
2022-05-13 16:15:20 +0530

    rv

--- src/components/Navbar/FullScreenNavbar.component.jsx
+++ src/components/Navbar/FullScreenNavbar.component.jsx
@@ -1,187 +1,191 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from "react";
 
 import {
-	FullScreenNavbarStyle,
-	NavLinksContainer,
-	Navlink,
-	MenuButton,
-	NavRight,
-	NavRightImages,
-	HoverContact
-} from './FullScreenNavbar.style';
+    FullScreenNavbarStyle,
+    NavLinksContainer,
+    Navlink,
+    MenuButton,
+    NavRight,
+    NavRightImages,
+    HoverContact,
+} from "./FullScreenNavbar.style";
 
-import img1 from '../../assets/nav/pic1.webp';
-import img2 from '../../assets/nav/pic2.webp';
-import img3 from '../../assets/nav/pic3.webp';
-import img4 from '../../assets/nav/pic4.webp';
-import img5 from '../../assets/nav/pic5.webp';
-import img6 from '../../assets/nav/pic6.webp';
+import Nav_logo from "../../assets/Trojans_logo/trojans_logo.webp";
+
+import img1 from "../../assets/nav/pic1.webp";
+import img2 from "../../assets/nav/pic2.webp";
+import img3 from "../../assets/nav/pic3.webp";
+import img4 from "../../assets/nav/pic4.webp";
+import img5 from "../../assets/nav/pic5.webp";
+import img6 from "../../assets/nav/pic6.webp";
 
 const NavLinkComponent = ({
-	body,
-	url,
-	activeRoute,
-	setScrollDiv,
-	scroll,
-	activeScroll,
-	color
+    body,
+    url,
+    activeRoute,
+    setScrollDiv,
+    scroll,
+    activeScroll,
+    color,
 }) => (
-	<Navlink
-		activeRoute={activeRoute}
-		onMouseEnter={() => setScrollDiv(scroll)}
-		onMouseLeave={() => setScrollDiv(activeScroll)}
-		body={body}
-		color={color}
-	>
-		<a href={url}>{body}&nbsp;</a>
-	</Navlink>
+    <Navlink
+        activeRoute={activeRoute}
+        onMouseEnter={() => setScrollDiv(scroll)}
+        onMouseLeave={() => setScrollDiv(activeScroll)}
+        body={body}
+        color={color}
+    >
+        <a href={url}>{body}&nbsp;</a>
+    </Navlink>
 );
 
-const FullScreenNavbar = ({ active, isNavOpen, setIsNavOpen }) => {
-	useEffect(() => {
-		if (isNavOpen) {
-			document.body.style.overflow = 'hidden';
-		} else {
-			document.body.style.overflow = 'auto';
-		}
-	}, [isNavOpen]);
+const FullScreenNavbar = ({active, isNavOpen, setIsNavOpen}) => {
+    useEffect(() => {
+        if (isNavOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [isNavOpen]);
 
-	const images = [
-		img2,
-		img3,
-		img1,
-		img4,
-		img5,
-		img6,
-		img2
-		// 'https://images.pexels.com/photos/11041919/pexels-photo-11041919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-		// 'https://images.pexels.com/photos/5818628/pexels-photo-5818628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-		// 'https://images.pexels.com/photos/8910626/pexels-photo-8910626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-		// 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-	];
+    const images = [
+        img2,
+        img3,
+        img1,
+        img4,
+        img5,
+        img6,
+        img2,
+        // 'https://images.pexels.com/photos/11041919/pexels-photo-11041919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // 'https://images.pexels.com/photos/5818628/pexels-photo-5818628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // 'https://images.pexels.com/photos/8910626/pexels-photo-8910626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        // 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        // 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        // 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        // 'https://images.pexels.com/photos/2166694/pexels-photo-2166694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    ];
 
-	const [scrollDiv, setScrollDiv] = useState(active.scroll);
+    const [scrollDiv, setScrollDiv] = useState(active.scroll);
 
-	return (
-		<FullScreenNavbarStyle isNavOpen={isNavOpen}>
-			<HoverContact>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 1))',
-						animationDelay: 'calc(-0.25s * 1)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 2))',
-						animationDelay: 'calc(-0.25s * 2)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 3))',
-						animationDelay: 'calc(-0.25s * 3)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 4))',
-						animationDelay: 'calc(-0.25s * 4)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 5))',
-						animationDelay: 'calc(-0.25s * 5)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 6))',
-						animationDelay: 'calc(-0.25s * 6)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 7))',
-						animationDelay: 'calc(-0.25s * 7)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 8))',
-						animationDelay: 'calc(-0.25s * 8)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg * 9))',
-						animationDelay: 'calc(-0.25s * 9)'
-					}}
-				></span>
-				<span
-					style={{
-						transform: 'rotate(calc(36deg *10))',
-						animationDelay: 'calc(-0.25s * 10)'
-					}}
-				></span>
-			</HoverContact>
-			<MenuButton onClick={() => setIsNavOpen(false)}>
-				<span></span>
-				<span></span>
-				<span></span>
-			</MenuButton>
-			<NavLinksContainer>
-				<NavLinkComponent
-					activeRoute={active.route === 'home' ? true : false}
-					url='/'
-					body='&nbsp;Home'
-					setScrollDiv={setScrollDiv}
-					scroll={0}
-					activeScroll={active.scroll}
-					color='red'
-				/>
-				<NavLinkComponent
-					activeRoute={active.route === 'events' ? true : false}
-					url='/events'
-					body='&nbsp;Events'
-					setScrollDiv={setScrollDiv}
-					scroll={1}
-					activeScroll={active.scroll}
-					color='blue'
-				/>
-				<NavLinkComponent
-					activeRoute={active.route === 'guidelines' ? true : false}
-					url='/guidelines'
-					body='&nbsp;Guidelines'
-					setScrollDiv={setScrollDiv}
-					scroll={2}
-					activeScroll={active.scroll}
-					color='green'
-				/>
-				<NavLinkComponent
-					activeRoute={active.route === 'sponsor' ? true : false}
-					url='/sponsor'
-					body='&nbsp;Sponsor'
-					setScrollDiv={setScrollDiv}
-					scroll={3}
-					activeScroll={active.scroll}
-					color='purple'
-				/>
-				<NavLinkComponent
-					activeRoute={active.route === 'registration' ? true : false}
-					url='/registration'
-					body='&nbsp;Register'
-					setScrollDiv={setScrollDiv}
-					scroll={4}
-					activeScroll={active.scroll}
-					color='yellow'
-				/>
-			</NavLinksContainer>
-			<NavRight>
+    return (
+        <FullScreenNavbarStyle isNavOpen={isNavOpen}>
+            <HoverContact>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 1))",
+                        animationDelay: "calc(-0.25s * 1)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 2))",
+                        animationDelay: "calc(-0.25s * 2)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 3))",
+                        animationDelay: "calc(-0.25s * 3)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 4))",
+                        animationDelay: "calc(-0.25s * 4)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 5))",
+                        animationDelay: "calc(-0.25s * 5)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 6))",
+                        animationDelay: "calc(-0.25s * 6)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 7))",
+                        animationDelay: "calc(-0.25s * 7)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 8))",
+                        animationDelay: "calc(-0.25s * 8)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg * 9))",
+                        animationDelay: "calc(-0.25s * 9)",
+                    }}
+                ></span>
+                <span
+                    style={{
+                        transform: "rotate(calc(36deg *10))",
+                        animationDelay: "calc(-0.25s * 10)",
+                    }}
+                ></span>
+            </HoverContact>
+            <MenuButton onClick={() => setIsNavOpen(false)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </MenuButton>
+            <NavLinksContainer>
+                <NavLinkComponent
+                    activeRoute={active.route === "home" ? true : false}
+                    url="/"
+                    body="&nbsp;Home"
+                    setScrollDiv={setScrollDiv}
+                    scroll={0}
+                    activeScroll={active.scroll}
+                    color="red"
+                />
+                <NavLinkComponent
+                    activeRoute={active.route === "events" ? true : false}
+                    url="/events"
+                    body="&nbsp;Events"
+                    setScrollDiv={setScrollDiv}
+                    scroll={1}
+                    activeScroll={active.scroll}
+                    color="blue"
+                />
+                <NavLinkComponent
+                    activeRoute={active.route === "guidelines" ? true : false}
+                    url="/guidelines"
+                    body="&nbsp;Guidelines"
+                    setScrollDiv={setScrollDiv}
+                    scroll={2}
+                    activeScroll={active.scroll}
+                    color="green"
+                />
+                <NavLinkComponent
+                    activeRoute={active.route === "sponsor" ? true : false}
+                    url="/sponsor"
+                    body="&nbsp;Sponsor"
+                    setScrollDiv={setScrollDiv}
+                    scroll={3}
+                    activeScroll={active.scroll}
+                    color="purple"
+                />
+                <NavLinkComponent
+                    activeRoute={active.route === "registration" ? true : false}
+                    url="/registration"
+                    body="&nbsp;Register"
+                    setScrollDiv={setScrollDiv}
+                    scroll={4}
+                    activeScroll={active.scroll}
+                    color="yellow"
+                />
+            </NavLinksContainer>
+
+            <NavRight></NavRight>
+            {/* <NavRight>
 				<div>
 					{images.map((image, index) => (
 						<NavRightImages src={image} scroll={scrollDiv} left key={index} />
@@ -192,9 +196,9 @@ const FullScreenNavbar = ({ active, isNavOpen, setIsNavOpen }) => {
 						<NavRightImages src={image} scroll={scrollDiv} key={index} />
 					))}
 				</div>
-			</NavRight>
-		</FullScreenNavbarStyle>
-	);
+			</NavRight> */}
+        </FullScreenNavbarStyle>
+    );
 };
 
 export default FullScreenNavbar;
--- src/components/Navbar/FullScreenNavbar.style.js
+++ src/components/Navbar/FullScreenNavbar.style.js
@@ -1,134 +1,137 @@
-import styled, { keyframes } from 'styled-components';
+import styled, {keyframes} from "styled-components";
+// import Nav_logo from "../../assets/Trojans_logo/trojans_logo.webp";
+import Nav_logo from "../../assets/Trojans_logo/trojans-compressed.webp";
 
 export const FullScreenNavbarStyle = styled.div`
-	position: fixed;
-	width: 100%;
-	height: 100vh;
-	top: 0;
-	background-color: black;
-	z-index: 1000;
-	display: flex;
-	color: white;
-	padding-left: 5rem;
-	justify-content: space-between;
-	overflow: hidden;
-	transition: all 0.3s ease-in-out;
-	opacity: ${({ isNavOpen }) => (isNavOpen ? 1 : 0)};
-	z-index: ${({ isNavOpen }) => (isNavOpen ? 101 : -1)};
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    background-color: black;
+    z-index: 1000;
+    display: flex;
+    color: white;
+    padding-left: 5rem;
+    justify-content: space-between;
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+    opacity: ${({isNavOpen}) => (isNavOpen ? 1 : 0)};
+    z-index: ${({isNavOpen}) => (isNavOpen ? 101 : -1)};
 
-	@media (max-width: 500px) {
-		padding-left: 2rem;
-	}
+    @media (max-width: 500px) {
+        padding-left: 2rem;
+    }
 `;
 
 export const NavLinksContainer = styled.ul`
-	display: flex;
-	align-items: flex-start;
-	justify-content: center;
-	list-style: none;
-	flex-direction: column;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    list-style: none;
+    flex-direction: column;
 `;
 
 export const Navlink = styled.li`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	margin-right: 1rem;
-	margin: 0.5rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 1rem;
+    margin: 0.5rem 0;
 
-	a {
-		font-size: 2rem !important;
-		text-decoration: none;
-		font-weight: bold;
-		color: transparent;
-		-webkit-text-stroke: 0.1px
-			${({ color, activeRoute }) =>
-				activeRoute ? color : 'rgba(255, 255, 255)'};
-		transition: all 0.2s ease-in;
-		position: relative;
-		font-family: 'ShareTechMono';
+    a {
+        font-size: 2rem !important;
+        text-decoration: none;
+        font-weight: bold;
+        color: transparent;
+        -webkit-text-stroke: 0.1px
+            ${({color, activeRoute}) =>
+                activeRoute ? color : "rgba(255, 255, 255)"};
+        transition: all 0.2s ease-in;
+        position: relative;
+        font-family: "ShareTechMono";
 
-		@media (max-width: 350px) {
-			
-			font-size: 2rem !important;
-		}
-	}
+        @media (max-width: 350px) {
+            font-size: 2rem !important;
+        }
+    }
 
-	a::before {
-		content: '${({ body }) => body}';
-		position: absolute;
-		color: ${({ color }) => color};
-		overflow: hidden;
-		width: ${({ activeRoute }) => (activeRoute ? '100%' : '0%')};
-		height: 100%;
-		transition: all 0.2s ease-in;
-		border-right: 5px solid ${({ color }) => color};
-		filter: drop-shadow(
-			${({ color, activeRoute }) =>
-				activeRoute ? '0 0 25px' : '0 0 0 ' + color}
-		);
-	}
+    a::before {
+        content: "${({body}) => body}";
+        position: absolute;
+        color: ${({color}) => color};
+        overflow: hidden;
+        width: ${({activeRoute}) => (activeRoute ? "100%" : "0%")};
+        height: 100%;
+        transition: all 0.2s ease-in;
+        border-right: 5px solid ${({color}) => color};
+        filter: drop-shadow(
+            ${({color, activeRoute}) =>
+                activeRoute ? "0 0 25px" : "0 0 0 " + color}
+        );
+    }
 
-	a:hover::before {
-		width: 100%;
-		-webkit-text-stroke: 1px ${({ color }) => color};
-		border-right: 4px solid ${({ color }) => color};
-		filter: drop-shadow(0 0 25px ${({ color }) => color});
-	}
+    a:hover::before {
+        width: 100%;
+        -webkit-text-stroke: 1px ${({color}) => color};
+        border-right: 4px solid ${({color}) => color};
+        filter: drop-shadow(0 0 25px ${({color}) => color});
+    }
 `;
 
 export const MenuButton = styled.div`
-	width: 3rem;
-	height: 2.5rem;
-	position: absolute;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	top: 3rem;
+    width: 3rem;
+    height: 2.5rem;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    top: 3rem;
 
-	span {
-		background-color: white;
-		width: 100%;
-		height: 2px;
-		border-radius: 10px;
-		transform-origin: center;
-		position: absolute;
-	}
+    span {
+        background-color: white;
+        width: 100%;
+        height: 2px;
+        border-radius: 10px;
+        transform-origin: center;
+        position: absolute;
+    }
 
-	span:nth-of-type(1) {
-		transform: rotate(-45deg);
-	}
+    span:nth-of-type(1) {
+        transform: rotate(-45deg);
+    }
 
-	span:nth-of-type(2) {
-		display: none;
-	}
+    span:nth-of-type(2) {
+        display: none;
+    }
 
-	span:nth-of-type(3) {
-		transform: rotate(45deg);
-	}
+    span:nth-of-type(3) {
+        transform: rotate(45deg);
+    }
 `;
 
 export const NavRight = styled.div`
-	width: 60%;
-	height: 100%;
-	position: relative;
-	display: flex;
+    width: 60%;
+    height: 100%;
+    position: relative;
+    display: flex;
+    background: url(${Nav_logo}) center/contain no-repeat;
+	padding: 2% 0;
 
-	div {
-		transition: all 0.5s ease-in-out;
-		position: relative;
-		height: 100%;
-		width: 50%;
-	}
+    div {
+        transition: all 0.5s ease-in-out;
+        position: relative;
+        height: 100%;
+        width: 50%;
+    }
 
-	div:nth-of-type(1) {
-		margin-right: 4rem;
-	}
+    div:nth-of-type(1) {
+        margin-right: 4rem;
+    }
 
-	@media (max-width: 900px) {
-		display: none;
-	}
+    @media (max-width: 900px) {
+        display: none;
+    }
 `;
 
 const navImageAnimation = keyframes`
@@ -143,17 +146,17 @@ const navImageAnimation = keyframes`
 `;
 
 export const NavRightImages = styled.div`
-	width: 100% !important;
-	height: 70vh !important;
-	position: relative;
-	background: url(${({ src }) => src}) center/cover;
-	margin: 4rem 0;
-	border-radius: 10px;
-	transform: translateY(
-		${({ scroll, left }) =>
-			(left ? -100 - scroll * 110 : -640 + scroll * 120) + '%'}
-	);
-	animation: ${navImageAnimation} 1s ease;
+    width: 100% !important;
+    height: 70vh !important;
+    position: relative;
+    background: url(${({src}) => src}) center/cover;
+    margin: 4rem 0;
+    border-radius: 10px;
+    transform: translateY(
+        ${({scroll, left}) =>
+            (left ? -100 - scroll * 110 : -640 + scroll * 120) + "%"}
+    );
+    animation: ${navImageAnimation} 1s ease;
 `;
 
 const hovereffectanimation = keyframes`
@@ -168,37 +171,37 @@ const hovereffectanimation = keyframes`
 `;
 
 export const HoverContact = styled.section`
-	position: absolute;
-	width: 200px;
-	height: 200px;
-	margin: 10%;
-	transition: 5s;
-	top: 10%;
-	right: -40%;
-	transition: 5s;
-	span {
-		position: absolute;
-		top: 0%;
-		left: 0%;
-		width: 100%;
-		height: 100%;
-	}
-	span::before {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 10px;
-		height: 10px;
-		border-radius: 50%;
-		background: transparent;
-		border: 4px solid #00efff;
-		box-sizing: border-box;
-		box-shadow: 0 0 20px #00efff, -200px -200px 0px yellow,
-			-200px -200px 20px yellow, 200px 200px 0px yellow, 200px 200px 20px yellow,
-			-200px 200px 0px #00efff, -200px 200px 0px red, 200px -200px 20px blue,
-			200px -200px 20px blue;
-		transform-origin: 250px;
-		animation: ${hovereffectanimation} 10s linear infinite;
-	}
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    margin: 10%;
+    transition: 5s;
+    top: 10%;
+    right: -40%;
+    transition: 5s;
+    span {
+        position: absolute;
+        top: 0%;
+        left: 0%;
+        width: 100%;
+        height: 100%;
+    }
+    span::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background: transparent;
+        border: 4px solid #00efff;
+        box-sizing: border-box;
+        box-shadow: 0 0 20px #00efff, -200px -200px 0px yellow,
+            -200px -200px 20px yellow, 200px 200px 0px yellow,
+            200px 200px 20px yellow, -200px 200px 0px #00efff,
+            -200px 200px 0px red, 200px -200px 20px blue, 200px -200px 20px blue;
+        transform-origin: 250px;
+        animation: ${hovereffectanimation} 10s linear infinite;
+    }
 `;
